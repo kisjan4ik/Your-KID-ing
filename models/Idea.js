192e8af30a4ecea34c690437b7cae73a7aea2f1d
Turetska <oturetska@yahoo.com>
2020-08-14 16:36:31 -0400

    vreated model and seed

---
+++ models/Idea.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const IdeaSchema = new Schema({
+    placename: 
+    {
+        type: String,
+        require: true
+    },
+    image:
+    {
+        type: String,
+        required: true
+    },
+    category: 
+    {
+        type: String,
+        require: true
+    },
+
+    where: 
+    {
+        type: String,
+        require: true
+    },
+    activeness: 
+    {
+        type: String,
+        require: true
+    },
+    age:
+     {
+        type: String,
+    },
+    address: 
+    {
+        type: String,
+        require: true
+    },
+    phone:
+    {
+        type: String,
+        require: true
+    },
+    website:
+    {
+        type: String,
+        require: true
+    }
+});
+
+module.exports = Idea = mongoose.model("Idea", IdeaSchema);
