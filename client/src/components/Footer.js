6a57792914eea58eabfd304ee946056f9b819286
Turetska <oturetska@yahoo.com>
2020-08-24 20:11:39 -0400

    folder roures edittions

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import logo from "../assets/images/logo.png";
+import logo from "../assets/images/logo.PNG";
 
 function Footer() {
 
